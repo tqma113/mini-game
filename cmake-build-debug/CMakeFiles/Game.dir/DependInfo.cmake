94aa8e6c4b108b2f0085a14853e14cbefde1ffd4
matianqi <mtq1997@126.com>
2019-11-07 00:35:17 +0800

    chore(cmake-build-debug)

---
+++ cmake-build-debug/CMakeFiles/Game.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/matianqi/Projects/mini-game/GameServer/game_server.c" "/Users/matianqi/Projects/mini-game/cmake-build-debug/CMakeFiles/Game.dir/GameServer/game_server.c.o"
+  "/Users/matianqi/Projects/mini-game/GameServer/init_info.c" "/Users/matianqi/Projects/mini-game/cmake-build-debug/CMakeFiles/Game.dir/GameServer/init_info.c.o"
+  "/Users/matianqi/Projects/mini-game/Webserver/http_session.c" "/Users/matianqi/Projects/mini-game/cmake-build-debug/CMakeFiles/Game.dir/Webserver/http_session.c.o"
+  "/Users/matianqi/Projects/mini-game/Webserver/init_log.c" "/Users/matianqi/Projects/mini-game/cmake-build-debug/CMakeFiles/Game.dir/Webserver/init_log.c.o"
+  "/Users/matianqi/Projects/mini-game/Webserver/init_web_server.c" "/Users/matianqi/Projects/mini-game/cmake-build-debug/CMakeFiles/Game.dir/Webserver/init_web_server.c.o"
+  "/Users/matianqi/Projects/mini-game/Webserver/webserver.c" "/Users/matianqi/Projects/mini-game/cmake-build-debug/CMakeFiles/Game.dir/Webserver/webserver.c.o"
+  "/Users/matianqi/Projects/mini-game/Websockets/base64.c" "/Users/matianqi/Projects/mini-game/cmake-build-debug/CMakeFiles/Game.dir/Websockets/base64.c.o"
+  "/Users/matianqi/Projects/mini-game/Websockets/init_websocket_server.c" "/Users/matianqi/Projects/mini-game/cmake-build-debug/CMakeFiles/Game.dir/Websockets/init_websocket_server.c.o"
+  "/Users/matianqi/Projects/mini-game/Websockets/init_ws_log.c" "/Users/matianqi/Projects/mini-game/cmake-build-debug/CMakeFiles/Game.dir/Websockets/init_ws_log.c.o"
+  "/Users/matianqi/Projects/mini-game/Websockets/session.c" "/Users/matianqi/Projects/mini-game/cmake-build-debug/CMakeFiles/Game.dir/Websockets/session.c.o"
+  "/Users/matianqi/Projects/mini-game/Websockets/sha1.c" "/Users/matianqi/Projects/mini-game/cmake-build-debug/CMakeFiles/Game.dir/Websockets/sha1.c.o"
+  "/Users/matianqi/Projects/mini-game/Websockets/websocket_server.c" "/Users/matianqi/Projects/mini-game/cmake-build-debug/CMakeFiles/Game.dir/Websockets/websocket_server.c.o"
+  "/Users/matianqi/Projects/mini-game/Websockets/websocket_session_init.c" "/Users/matianqi/Projects/mini-game/cmake-build-debug/CMakeFiles/Game.dir/Websockets/websocket_session_init.c.o"
+  "/Users/matianqi/Projects/mini-game/game.c" "/Users/matianqi/Projects/mini-game/cmake-build-debug/CMakeFiles/Game.dir/game.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Game.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Game.dir/game.c.o"
+  "CMakeFiles/Game.dir/Webserver/webserver.c.o"
+  "CMakeFiles/Game.dir/Webserver/init_web_server.c.o"
+  "CMakeFiles/Game.dir/Webserver/init_log.c.o"
+  "CMakeFiles/Game.dir/Webserver/http_session.c.o"
+  "CMakeFiles/Game.dir/Websockets/websocket_server.c.o"
+  "CMakeFiles/Game.dir/GameServer/game_server.c.o"
+  "CMakeFiles/Game.dir/Websockets/init_websocket_server.c.o"
+  "CMakeFiles/Game.dir/Websockets/init_ws_log.c.o"
+  "CMakeFiles/Game.dir/Websockets/websocket_session_init.c.o"
+  "CMakeFiles/Game.dir/Websockets/base64.c.o"
+  "CMakeFiles/Game.dir/Websockets/sha1.c.o"
+  "CMakeFiles/Game.dir/GameServer/init_info.c.o"
+  "CMakeFiles/Game.dir/Websockets/session.c.o"
+  "Game.pdb"
+  "Game"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
